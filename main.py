a3b68b92b82502586c96d9734ce7f7459ea69aee
Rohan-V <rohankvij@gmail.com>
2023-04-08 16:41:07 -0700

    Added the scan form

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 from dotenv import load_dotenv
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 from text import send_twilio_message
@@ -18,12 +18,44 @@ client = MongoClient(f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASS}"
                      "@main.hup8pvq.mongodb.net/?retryWrites=true&w=majority")
 
 db = client.development
-users = db.users
+reports = db.reports
 
 @app.route("/")
 def home():
     """Home page of the application."""
     return render_template("home.html")
+
+@app.route("/scan", methods=["GET", "POST"])
+def scan():
+    """Scan a plant."""
+    if request.method == "GET":
+        return render_template("scan.html")
+ 
+    elif request.method == "POST":
+        phoneno = request.form.get("phoneno")
+        latitude = request.form.get("latitude")
+        longitude = request.form.get("longitude")
+        # disease = request.form.get("disease")
+
+        print(latitude, longitude)
+
+        report = {
+            "phoneno": phoneno,
+            "latitude": latitude,
+            "longitude": longitude,
+            # "disease": disease
+        }
+
+        reports.insert_one(report)
+
+        send_twilio_message("Your plant has been scanned! Thank you for your input.", phoneno.replace(" ", ""))
+
+        return render_template("scan.html")
+
+@app.route("/map")
+def display_map():
+    """Map of plants."""
+    return render_template("map.html")
 
 if __name__ == "__main__":
     app.run()
