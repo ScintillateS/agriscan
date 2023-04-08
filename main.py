e578fea4a834ef40d88fb00061e2a5b7d46d15d9
Rohan-V <rohankvij@gmail.com>
2023-04-08 13:21:53 -0700

    Added home page

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from dotenv import load_dotenv
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -12,15 +12,16 @@ load_dotenv()
 MONGODB_USER = os.environ.get('MONGODB_USER')
 MONGODB_PASS = os.environ.get('MONGODB_PASS')
 
-client = MongoClient(f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASS}@main.hup8pvq.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient(f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASS}"
+                     "@main.hup8pvq.mongodb.net/?retryWrites=true&w=majority")
 
 db = client.development
 users = db.users
 
 @app.route("/")
-def main():
+def home():
     """Home page of the application."""
-    return "Hello World!"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run()
