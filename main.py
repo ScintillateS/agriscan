89de18e434950ea824781d27c3b4a13c9e405240
Rohan-V <rohankvij@gmail.com>
2023-04-08 10:25:39 -0700

    Added the PyMongo interface

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 """Main entrypoint of the application."""
+from dotenv import load_dotenv
+import os
+
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+load_dotenv()
+
+MONGODB_USER = os.environ.get('MONGODB_USER')
+MONGODB_PASS = os.environ.get('MONGODB_PASS')
+
+client = MongoClient(f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASS}@main.hup8pvq.mongodb.net/?retryWrites=true&w=majority")
+
+db = client.development
+users = db.users
 
 @app.route("/")
 def main():
