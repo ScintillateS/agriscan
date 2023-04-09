7151797cd301d8ec9c04ad0172e108a961b8a6e5
Rohan-V <rohankvij@gmail.com>
2023-04-08 21:02:25 -0700

    Updated text message

--- main.py
+++ main.py
@@ -191,7 +191,7 @@ def scan():
         reports.insert_one(report)
 
         send_twilio_message(
-            f"Your plant has been scanned! Thank you for your input. Your {result[0]} was identified with: {result[1]}. It is {'healthy :)' if result[2] else 'unhealthy :('}", phoneno.replace(" ", ""))
+            f"Your plant has been scanned! Thank you for your input.\n Your {result[0]} was identified with: {result[1]}. It is {'healthy' if result[2] else 'unhealthy'}.", phoneno.replace(" ", ""))
 
         os.remove(path)
 
